2591debdfb6dbaa6857cc280cc9228d0ec46e76f
liuxingjun <475806058@qq.com>
2023-02-23 12:32:30 +0800

    add log

--- src/catalogs.js
+++ src/catalogs.js
@@ -1,16 +1,43 @@
-import { readJSON, writeCSV } from 'https://deno.land/x/flat/mod.ts'
-import _ from 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/+esm'
+import { readJSON, writeCSV } from 'https://deno.land/x/flat@0.0.15/mod.ts'
+import { parse } from "https://deno.land/std@0.175.0/flags/mod.ts";
+import * as log from "https://deno.land/std@0.177.0/log/mod.ts";
 
-// The filename is the first invocation argument
+//region 获取参数
+const flags = parse(Deno.args, {
+    string: ["log"],
+    default: { "log": "info" },
+});
+console.debug("flags:", flags);
+//endregion
 
+// Deno.exit(5);
+await log.setup({
+    handlers: {
+        default: new log.handlers.ConsoleHandler("DEBUG"),
+    },
+    loggers: {
+        default: {
+            level: flags.log.toUpperCase(),
+            handlers: ["default"],
+        },
+    },
+});
 const filename = Deno.args[0] // Same name as downloaded_filename
-const data = await readJSON(filename)
-// console.debug(data[4].subCategories[1])
-var csvCategorylist = []
+let data = []
+try {
+    data = await readJSON(filename)
+    log.debug("read success to" + filename)
+} catch (error) {
+    log.error('read error to ' + filename + ' error:' + error.message);
+    Deno.exit(1)
+}
+
+// log.debug(data[4].subCategories[1])
+const csvCategorylist = []
 function flat(categories, parent_id, depth) {
     for (const category of categories) {
-        // console.log(category.name)
-        var newCategory = {}
+        // log.debug(category.name)
+        const newCategory = {}
         newCategory.id = category.id
         newCategory.name = category.name
         newCategory.parent_id = parent_id
@@ -25,8 +52,10 @@ function flat(categories, parent_id, depth) {
 }
 flat(data, 0, 1)
 // console.debug(csvCategorylist.length)
-// Pluck a specific key off
-// and write it out to a different file
-// Careful! any uncaught errors and the workflow will fail, committing nothing.
 const newfile = `./data/catalogs.csv`
-await writeCSV(newfile, csvCategorylist)
+try {
+    await writeCSV(newfile, csvCategorylist)
+    log.debug('write success to ' + newfile);
+} catch (error) {
+    log.error('write error to ' + newfile + " error :" + error.message);
+}
